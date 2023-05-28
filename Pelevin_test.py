fa06e90d882821c7a59696665eaa49d64137d930
Anna <anna.gosteva@gmx.fr>
2023-05-28 22:45:18 +0200

    First commit

---
+++ Pelevin_test.py
@@ -0,0 +1,23 @@
+import spacy 
+CHAPTERS = [
+    "ГОЛЬДЕНШТЕРН ВСЕ",
+    "ПОЕДИНОК",
+    "СВИДЕТЕЛЬ ПРЕКРАСНОГО",
+    "БРО КУКУРАТОР",
+    "КОШЕЧКА",
+    "МИТИНА ЛЮБОВЬ",
+    "HOMO OVERCLOCKED"
+]
+сhapters = []
+with open("./data/Pelevin.txt", "r") as f:
+    text = f.read()
+    chap = text.split(CHAPTERS[0]) [1:]
+
+chapter1 = chap[0]
+nlp = spacy.load("ru_core_news_lg")
+
+doc = nlp(chapter1)
+sentences = list(doc.sents)
+# print(sentences[1])
+for line in sentences:
+    print(line)
